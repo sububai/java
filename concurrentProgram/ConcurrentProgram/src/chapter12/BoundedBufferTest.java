a7f94f59944a79dc68979028b5a7d63b1d2d7424
guan haishui <1058661149@qq.com>
2018-01-05 15:39:01 +0800

    并发程序的测试
    简单看看

---
+++ concurrentProgram/ConcurrentProgram/src/chapter12/BoundedBufferTest.java
@@ -0,0 +1,19 @@
+package chapter12;
+
+import junit.framework.TestCase;
+
+public class BoundedBufferTest extends TestCase {
+	void testIsEmptyWhenConstructed() {
+		BoundedBuffer<Integer> bb = new BoundedBuffer<Integer>(10);
+		assertTrue(bb.isEmpty());
+		assertFalse(bb.isFull());
+	}
+	
+	void testIsFullAfterPuts() throws InterruptedException {
+		BoundedBuffer<Integer> bb = new BoundedBuffer<Integer>(10);
+		for (int i = 0; i < 10; i++)
+			bb.put(i);
+		assertTrue(bb.isFull());
+		assertFalse(bb.isEmpty());
+	}
+}
