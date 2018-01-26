8287c4c3aa464f0a545b33e84cfcb4d4ff6ef2b8
guan haishui <1058661149@qq.com>
2018-01-26 15:48:08 +0800

    Revert "xml和Java配置对比"
    This reverts commit d915a058ee21411c0d1c2c209ac24a8ec91396a6.

--- Spring In Action/Spittr/src/spittr/config/SpittrWebAppInitializer.java
+++ Spring In Action/Spittr/src/spittr/config/SpittrWebAppInitializer.java
@@ -2,11 +2,6 @@ package spittr.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import com.myapp.MyFilter;
-
-import javax.servlet.Filter;
-
-//java配置spring mvc，包括DispatcherServlet和ContextLoaderListener
 public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
@@ -27,8 +22,5 @@ public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherS
 		return new String[] { "/" };
 	}
 
-	@Override
-	protected Filter[] getServletFilters() {
-		return new Filter[] { new MyFilter() };
-	}
+	
 }
